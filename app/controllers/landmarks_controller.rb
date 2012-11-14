be1394b620ec5db0c56c0dcb464e86d568b9d3fb
carsy <jlageb@gmail.com>
2012-11-14 11:31:59 +0000

    creating db2

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,7 @@
+require 'json' 
+require 'net/http'
+require 'uri'
+
 class LandmarksController < ApplicationController
   # GET /landmarks
   # GET /landmarks.json
@@ -14,6 +18,8 @@ class LandmarksController < ApplicationController
   # GET /landmarks/1.json
   def show
     @landmark = Landmark.find(params[:id])
+    
+    # TODO change json rendering to match fb struture
 
     respond_to do |format|
       format.html # show.html.erb
@@ -42,9 +48,19 @@ class LandmarksController < ApplicationController
   def create
     @landmark = Landmark.new(params[:landmark])
 
+    uri = URI.parse("http://graph.facebook.com/" + @landmark.username) # TODO: refactor 
+
+    response = Net::HTTP.get_response(uri)
+    http = Net::HTTP.new(uri.host, uri.port)
+    
+    response = http.request(Net::HTTP::Get.new(uri.request_uri))
+    @response = JSON.parse response.body
+    
+    @landmark.name = @response['name']
+
     respond_to do |format|
       if @landmark.save
-        format.html { redirect_to @landmark, notice: 'Landmark was successfully created.' }
+        format.html { redirect_to @landmark, notice: 'Landmark was successfully fetched.' }
         format.json { render json: @landmark, status: :created, location: @landmark }
       else
         format.html { render action: "new" }
